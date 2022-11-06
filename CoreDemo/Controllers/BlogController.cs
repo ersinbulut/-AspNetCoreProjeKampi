4de1464381c5bdbdf8f39705bbf0abec0f33666d
ersinbulut <ersinblt61@gmail.com>
2022-11-06 11:23:00 +0300

    #54 Asp.Net Core 5.0 Proje Kampı Yazarın Kendine Ait Blog Listesi

--- CoreDemo/Controllers/BlogController.cs
+++ CoreDemo/Controllers/BlogController.cs
@@ -20,6 +20,12 @@ namespace CoreDemo.Controllers
             var values = bm.GetBlogByID(id);
             return View(values);
         }
-        
+        public IActionResult BlogListByWriter()
+        {
+            var values = bm.GetBlogByWriter(1);
+            return View(values);
+        }
+
+
     }
 }
